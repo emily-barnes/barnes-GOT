f32da54cb854105dbc2b7ca8c7bd1e565a0a1070
Emily <emilybarnes@Emilys-MacBook-Pro.local>
2021-04-17 20:16:52 -0400

    finished refractering

--- js/main.js
+++ js/main.js
@@ -2,21 +2,22 @@
 	console.log('fired');
 
 
-	const sigils = document.querySelectorAll('.sigilContainer'),
+	const sigils = document.querySelector('#navList'),
 		  banner = document.querySelector('#houseImages');
 
 	// functions go here
 	function animateBanner(event) {
-		let multiplier = this.dataset.offset,
+		if (event.target.className.includes('sigilContainer')) {
+		let multiplier = event.target.dataset.offset,
 			offset = 600;
 
 
 		banner.style.right = `${multiplier * offset}px`;
 
-
+	}
 		
 	}
 
-
-	sigils.forEach(sigil => sigil.addEventListener('click', animateBanner));
+	//sigils.forEach(sigil => sigil.addEventListener('click', animateBanner));
+	sigils.addEventListener('click', animateBanner);
 })();
